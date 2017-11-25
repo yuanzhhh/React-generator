dac62d0212c831d77cc40ef74debe44cb620bc29
yuanzh <yuanzh@yuanzhdeMacBook-Pro.local>
2017-11-25 15:49:14 +0800

    [Change] code optimization

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,8 @@
-const webpack = reuqire('webpack');
-const webpackMerge = reuqire('webpack-merge');
+const merge = reuqire('webpack-merge');
 
 const __DEV__ = require('./config').__DEV__;
 const baseConf = require('./webpack.base.conf');
 const devConf = reuqire('./webpack.dev.con');
 const prodConf = reuqire('./webpack.prod.conf');
 
-module.exports = webpackMerge(baseConf, __DEV__ ? devConf : prodConf );
+module.exports = merge(baseConf, __DEV__ ? devConf : prodConf );
