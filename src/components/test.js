0614fab3788d03a3b7ca93c19da3bd54fd9cde48
yuanzh <yuanzh@yuanzhdeMacBook-Pro-4.local>
2017-12-05 00:05:05 +0800

    [Change] 验证思想

--- src/components/test.js
+++ src/components/test.js
@@ -31,37 +31,38 @@ export default class Test extends React.Component {
 
   render () {
 
-        const obj = { a: 5, b: { a: 2 } };
+        const obj = { 
+            b: {
+              a: {
+                b: 1,
+              }
+            },
+            c: {
+              b: [1,2,3],
+            }
+          };
     
-        const a = update(obj, {b: {$set: {a: obj.b.a * 2} }});
-        const b = update(obj, {b: {a: {$set: obj.b.a * 2 }}});
-        const c = update(obj, {b: {$set: {a: obj.b.a * 2} }});
-    
-        // obj.b.a = obj.b.a * 2;
+        const a = update(obj, {b: {$set: {a: { b:123 }} }});
+        const b = update(obj, {b: {$set: {a: { b:123 }} }});
+
+        const c = update(obj, {c: {b: {$push: [4, 5]} }});
+
+        const d = update(c, {c: {b: {$push: [6, 7]} }});
+
+        // obj.b.a = { b: 123 };
         // const a = obj;
-        // obj.b.a = obj.b.a * 2;
+        // obj.b.a = { b: 123 };
         // const b = obj;
-        // obj.b.a = obj.b.a * 2;
-        // const c = obj;
 
-        console.log('a.a === b.a', a.a === b.a);
-        console.log('a.b === b.b', a.b === b.b);
-    
-        console.log('a === b', a === b);
-        console.log('c === b', c === b);
-        console.log('c === a', c === a);
-    
-        console.log('c.a === a.a', c.a === a.a);
-        console.log('c.b === a.b', c.b === a.b);
+        console.log(a.b === b.b);
+        console.log(a.c === b.c);
+        
+        console.log(c.b === a.b);
+        console.log(c.c === a.c);
+        console.log(c.c === a.c);
 
-        console.log(a, b, c);
-
-        const q = update(a, {b: {$set: {a: obj.b.a * 5} }});
-
-        console.log('a === q', a === q);
-        console.log('a.a === q.a', a.a === q.a);
-        console.log('a.b === q.b', a.b === q.b);
-
+        console.log(d.b === a.b);
+        console.log(d.b === c.b);
 
     return (
       <div>
