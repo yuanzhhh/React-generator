e9b499027704407d22bc245c39b52345078bf4d3
yuanzh <yuanzh@yuanzhdeMacBook-Pro-5.local>
2017-12-21 15:59:36 +0800

    style(next): [Optimize]
    优化代码

--- src/router/index.js
+++ src/router/index.js
@@ -6,20 +6,19 @@ import {
 } from 'react-router-dom';
 
 import Test from '../components/test';
-import Tests from '../components/tests';
+import DishMenu from '../views/dish-menu';
 
 const BasicRouter = props => (
   <Router history={props.history}>
     <div>
       <ul>
         <li><Link to="/">index</Link></li>
-        <li><Link to="/tests">tests</Link></li>
+        <li><Link to="/DishMenu">DishMenu</Link></li>
       </ul>
       <Route path="/" exact component={ Test } />
-      <Route path="/tests" component={ Tests } />
+      <Route path="/DishMenu" component={ DishMenu } />
     </div>
   </Router>
 )
-
 
 export default BasicRouter;
--- src/views/dish-menu/index.js
+++ src/views/dish-menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './test.scss';
+import './dist-menu.scss';
 
 export default class Tests extends React.Component {
   constructor (props) {
