f7e6fed02d95862663e2734433bcc27ab99c84a5
yuanzh <yuanzh@yuanzhdeMacBook-Pro-5.local>
2017-12-26 17:22:27 +0800

    fix(dishmenu-route): [Fix]
    修复一个警告问题

--- src/views/dish-menu/route.js
+++ src/views/dish-menu/route.js
@@ -3,12 +3,15 @@ import Loadable from 'react-loadable';
 import loading from '@/components/loadableLoading';
 import dispatchToProps from './dispatchToProps';
 
+const DishMenu = Loadable({
+    loader: () => import('./'),
+    loading,
+});
+
+
 export default {
     path: '/DishMenu',
-    component: Loadable({
-        loader: () => import('./'),
-        loading,
-    }),
+    component: DishMenu,
     init: async store => {
         await store.dispatch(dispatchToProps.getInitData(599))
         
